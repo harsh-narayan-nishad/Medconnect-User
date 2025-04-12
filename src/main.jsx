482843d7af956f29fa887adfad0423da827b3b5a
Harsh_Narayan <harshnarayannishad@gmail.com>
2025-04-12 06:46:14 +0530

    Prasoon's work : Search forwarding issue fixed

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { FormProvider } from './context/FormContext.jsx'
+
 
 
 
@@ -10,8 +10,8 @@ import { FormProvider } from './context/FormContext.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   
   <React.StrictMode>
-    <FormProvider>
+
       <App />
-    </FormProvider>
+
   </React.StrictMode>,
 )

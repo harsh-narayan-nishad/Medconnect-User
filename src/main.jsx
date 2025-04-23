d0a2aad881d790c20d6cf02d51556d71f32949ed
PrasoonRawat <prasoon818@gmail.com>
2025-04-23 07:56:47 +0530

    added the toaster and appointment booking

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
+import { Toaster } from './components/ui/toaster.jsx'
 
 
 
@@ -12,6 +12,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
 
       <App />
-
+      <Toaster/>
   </React.StrictMode>,
 )

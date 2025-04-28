0f424450b1637c57c91c13ab087b32018d732c4b
PrasoonRawat <prasoon818@gmail.com>
2025-04-28 21:19:36 +0530

    added hashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Toaster } from './components/ui/toaster.jsx'
-
+import { HashRouter } from 'react-router-dom'
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   
-  <React.StrictMode>
-
-      <App />
-      <Toaster/>
-  </React.StrictMode>,
+  <>
+      <HashRouter>
+        <App />
+        <Toaster/>
+      </HashRouter>
+  </>,
 )

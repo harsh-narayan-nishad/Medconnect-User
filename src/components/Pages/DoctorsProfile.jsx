ba1c3caa3a9af865cdaafda8f67cc545b33b3c7d
PrasoonRawat <prasoon818@gmail.com>
2025-04-28 17:53:32 +0530

    story bug fixed

--- src/components/Pages/DoctorsProfile.jsx
+++ src/components/Pages/DoctorsProfile.jsx
@@ -232,7 +232,7 @@ const handleSubmitStory = async () => {
       body: JSON.stringify({
         doctorId,
         visitedFor,
-        recommend,
+        recommended: recommend,
         story: storyText,
       }),
     });

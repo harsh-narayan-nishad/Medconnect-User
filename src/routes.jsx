777743f6dc130f6580550a9fb4cf0ac7220abf85
PrasoonRawat <prasoon818@gmail.com>
2025-05-08 08:16:50 +0530

    added user Page

--- src/routes.jsx
+++ src/routes.jsx
@@ -7,11 +7,8 @@ import UserDashboard from "./components/Pages/UserDashboard";
 import AppointmentSchedule from "./components/Pages/AppointmentSchedule";
 import ShareQr from "./components/ShareQrCode";
 import DoctorProfile from "./components/Pages/DoctorsProfile";
-<<<<<<< HEAD
 import UserProfile from "./components/Pages/UserProfile";
-=======
 import VideoConfrence from "./components/VideoConfrence";
->>>>>>> f1aa93604b3578966f15d0dc49f99e549e25225d
 
 const AppRoutes = () => {
   return (
@@ -25,13 +22,8 @@ const AppRoutes = () => {
         <Route path = "appointment" element={<AppointmentSchedule />} />
         <Route path = "qr-code-sharing" element = {<ShareQr />} />
         <Route path="/doctors-page/:id" element={<DoctorProfile />} />
-<<<<<<< HEAD
         <Route path="/User-page/:id" element={<UserProfile />} />
- 
-=======
         <Route path = "/video" element ={<VideoConfrence/>} />
-
->>>>>>> f1aa93604b3578966f15d0dc49f99e549e25225d
       </Routes>
     </>
   );

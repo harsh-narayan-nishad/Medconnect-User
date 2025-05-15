7e1bdec4889196617ee1a3cb79fe73d478921768
Harsh_Narayan <harshnarayannishad@gmail.com>
2025-05-15 23:17:56 +0530

    Introducing Delete Option : Documents

--- src/components/DocumentsSection.jsx
+++ src/components/DocumentsSection.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Pdfupload from "./PdfUpload";
+import styled from "styled-components";
+
 
 const DocumentsSection = () => {
     const [documents, setDocuments] = useState([]);
@@ -44,6 +46,7 @@ const DocumentsSection = () => {
     };
 
     return (
+
         <div className="bg-white rounded-xl p-6">
             <Pdfupload />
             <h2 className="text-xl font-semibold mb-4 text-gray-700">Your Medical Documents</h2>
@@ -80,7 +83,32 @@ const DocumentsSection = () => {
                                 )}
                             </a>
                             <div className="p-2 bg-gray-50 text-sm text-gray-700 font-semibold">
-                                Author: <span className="font-normal">{doc.author.fullname}</span>
+
+                                <div className="flex items-center justify-between w-full">
+                                    <span className="font-normal">Author: {doc.author.fullname}</span>
+                                    <StyledWrapper>
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            width={22}
+                                            height={22}
+                                            viewBox="0 0 24 24"
+                                            fill="none"
+                                            stroke="currentColor"
+                                            strokeWidth="1.5"
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            className="lucide lucide-trash-2 cursor-pointer"
+                                        >
+                                            <path d="M3 6h18" />
+                                            <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
+                                            <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" />
+                                            <line x1={10} x2={10} y1={11} y2={17} />
+                                            <line x1={14} x2={14} y1={11} y2={17} />
+                                        </svg>
+                                    </StyledWrapper>
+                                </div>
+
+
                             </div>
                         </div>
                     ))}
@@ -88,7 +116,210 @@ const DocumentsSection = () => {
 
             )}
         </div>
+
     );
 };
+const StyledWrapper = styled.div`
+.card {
+    background: #222222;
+    width: 260px;
+    border: 2px solid #313131;
+    border-radius: 10px;
+    padding: 3px 4px;
+
+    .separator {
+      width: 100%;
+      border: 1px solid #444444;
+      border-radius: 10px;
+      margin: 5px 0px;
+    }
+
+    .list {
+      color: #e9e9e9;
+      list-style-type: none;
+      display: flex;
+      flex-direction: column;
+      gap: 3px;
+
+      .item {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        transition: all 0.3s ease;
+        padding: 6px 8px;
+        border-radius: 5px;
+        cursor: pointer;
+        position: relative;
+        overflow: hidden;
+        user-select: none;
+
+        svg {
+          z-index: 1;
+          transition: all 0.3s ease;
+        }
+        &:hover {
+          background: #333333;
+        }
+
+        .label {
+          font-weight: 400;
+          transition: all 0.2s ease;
+        }
+
+        &.favorite {
+          .fav-label {
+            position: absolute;
+            transform: translateY(-100%) translateX(-15px) scale(0.8);
+            opacity: 0;
+          }
+
+          .input {
+            width: 100%;
+            height: 100%;
+            position: absolute;
+            appearance: none;
+            cursor: pointer;
+            z-index: 100;
+          }
+
+          .input:checked ~ .fav-label {
+            transform: translateY(0);
+            opacity: 1;
+          }
+
+          .input:checked ~ .label:not(.fav-label) {
+            transform: translateY(110%) translateX(-6px) scale(0.8);
+            opacity: 0;
+          }
+          .input:checked ~ svg {
+            fill: #fff;
+          }
+        }
+
+        &.delete {
+          color: #e3616a;
+          position: relative;
+          &:hover {
+            background: #6b2c2b;
+          }
+
+          .label {
+            transform: translateY(0);
+          }
+
+          &:active {
+            .label {
+              opacity: 0;
+              visibility: hidden;
+              transform: translateY(100%) translateX(-15px) scale(0.8);
+            }
+            .action {
+              opacity: 1;
+              visibility: visible;
+              transform: translateY(0);
+            }
+
+            &:before {
+              animation: delete 2.5s ease-in-out forwards 0.2s;
+            }
+          }
+
+          .action {
+            position: absolute;
+            opacity: 0;
+            visibility: hidden;
+            transform: translateY(-50%) translateX(-15px) scale(0.8);
+          }
+
+          &::before {
+            content: "";
+            position: absolute;
+            background-color: #89302d;
+            left: 0;
+            top: 0;
+            height: 100%;
+          }
+        }
+
+        &.rename {
+          &:has(.toogler:checked:nth-of-type(1)) {
+            background-color: #333333;
+            overflow: hidden;
+            .label {
+              opacity: 0;
+              transform: translateY(100%);
+            }
+            > svg {
+              transform: translateY(130%);
+            }
+
+            .input-container {
+              transform: translateY(0);
+
+              .icons {
+                top: 50%;
+              }
+            }
+          }
+
+          .toogler {
+            position: absolute;
+            appearance: none;
+            width: 100%;
+            height: 100%;
+          }
+
+          .input-container {
+            transform: translateY(-100%);
+            width: 100%;
+            position: absolute;
+
+            .input {
+              width: 75%;
+              background: transparent;
+              border: none;
+              outline: none;
+              height: 100%;
+              color: #fff;
+              padding: 8px 8px;
+              font-size: 17px;
+              z-index: 100;
+            }
+
+            .icons {
+              position: absolute;
+              top: -50%;
+              right: 0%;
+              transform: translate(-50%, -50%);
+              display: flex;
+              justify-content: center;
+              align-items: center;
+              transition: all 0.3s ease;
+              gap: 8px;
+              svg {
+                background-color: #565656;
+                border-radius: 4px;
+                cursor: pointer;
+                transition: all 0.3s ease;
+                &:hover {
+                  background-color: #757575;
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  @keyframes delete {
+    from {
+      width: 0%;
+    }
+
+    to {
+      width: 100%;
+    }
+`
 
 export default DocumentsSection;
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,6 @@
 import {Routes, Route } from "react-router-dom";
 import Home from "./components/Pages/Home";
 import Login from "./components/LoginSignup/Login";
-import DoctorLogin from "./components/LoginSignup/DoctorLogin";
 import Signup from "./components/LoginSignup/SignUp";
 import DoctorSearch from "./components/Pages/DoctorSearch";
 import UserDashboard from "./components/Pages/UserDashboard";
@@ -17,7 +16,7 @@ const AppRoutes = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/login-doctor" element={<DoctorLogin />} />
+
         <Route path="/signup" element={<Signup />} />
         <Route path ="doctor-search" element={<DoctorSearch/>} />
         <Route path = "user-dashboard/:username" element={<UserDashboard/>} />
